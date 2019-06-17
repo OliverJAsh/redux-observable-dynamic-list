b8929ac40cce80e132e2fb4db527b785fed9bc7e
Oliver Joseph Ash <oliverjash@gmail.com>
2019-06-17 15:19:45 +0100

    Solution: Re-using actions

--- src/epics.ts
+++ src/epics.ts
@@ -1,8 +1,42 @@
 import { Epic } from "redux-observable";
-import { EMPTY } from "rxjs";
-import { Action } from "./actions";
-import { State } from "./state-types";
+import { interval } from "rxjs";
+import { tag } from "rxjs-spy/operators/tag";
+import { filter, map, mapTo, mergeMap, takeUntil } from "rxjs/operators";
+import {
+  Action,
+  checkIsAddCounterAction,
+  checkIsRemoveCounterAction,
+  incrementCounter
+} from "./actions";
+import { getStateObservable } from "./redux-observable";
+import { CounterState, State } from "./state-types";
 
-export const rootEpic: Epic<Action, Action, State> = (_action$, _state$) => {
-  return EMPTY;
+const counterEpic: Epic<Action, Action, CounterState> = (_action$, state$) =>
+  interval(1000).pipe(
+    tag(`interval ${state$.value.id}`),
+    mapTo(incrementCounter(state$.value.id))
+  );
+
+export const rootEpic: Epic<Action, Action, State> = (action$, state$) => {
+  const addCounterAction$ = action$.pipe(filter(checkIsAddCounterAction));
+  const removeCounterAction$ = action$.pipe(filter(checkIsRemoveCounterAction));
+  const counterAction$ = addCounterAction$.pipe(
+    // Run one child epic per counter
+    mergeMap(action => {
+      const removeThisCounterAction$ = removeCounterAction$.pipe(
+        filter(({ id }) => id === action.id)
+      );
+      const getState = (state: State) => state.counterStates[action.id];
+      const initialState = getState(state$.value);
+      const counterStateObservable = getStateObservable(
+        state$.pipe(map(getState)),
+        initialState
+      );
+      return counterEpic(action$, counterStateObservable, {}).pipe(
+        // Dynamically unsubscribe from the child epic
+        takeUntil(removeThisCounterAction$)
+      );
+    })
+  );
+  return counterAction$;
 };
