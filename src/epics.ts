6b859af7bf848868d5669470e36df35da329ad65
Oliver Joseph Ash <oliverjash@gmail.com>
2019-06-17 15:21:31 +0100

    Solution: Watching the state

--- src/epics.ts
+++ src/epics.ts
@@ -1,8 +1,25 @@
 import { Epic } from "redux-observable";
-import { EMPTY } from "rxjs";
-import { Action } from "./actions";
-import { State } from "./state-types";
+import { interval } from "rxjs";
+import { tag } from "rxjs-spy/operators/tag";
+import { map, mapTo } from "rxjs/operators";
+import { Action, incrementCounter } from "./actions";
+import { runListEpics } from "./run-list-epics";
+import { CounterState, State } from "./state-types";
 
-export const rootEpic: Epic<Action, Action, State> = (_action$, _state$) => {
-  return EMPTY;
+const counterEpic: Epic<Action, Action, CounterState> = (_action$, state$) =>
+  interval(1000).pipe(
+    tag(`interval ${state$.value.id}`),
+    mapTo(incrementCounter(state$.value.id))
+  );
+
+export const rootEpic: Epic<Action, Action, State> = (action$, state$) => {
+  const counterAction$ = state$.pipe(
+    map(state => state.counterStates),
+    runListEpics({
+      action$,
+      listItemEpic: counterEpic,
+      selectListItem: id => counterStates => counterStates[id]
+    })
+  );
+  return counterAction$;
 };
---
+++ src/run-list-epics.ts
@@ -0,0 +1,100 @@
+// This helper allows us to run individual epics for each item in a dynamic list (e.g. files in the
+// uploader).
+
+// When the list item is added to the list state, the epic will be start running.
+
+// When the list item is removed from the list state, the epic will be unsubscribed from, thereby
+// aborting any pending work (e.g. requests).
+
+import { difference, mapValues, omit } from "lodash";
+import { Action as ReduxAction } from "redux";
+import { ActionsObservable, Epic } from "redux-observable";
+import { Observable, OperatorFunction, pipe } from "rxjs";
+import { mergeHigherOrderArray } from "rxjs-etc/observable/mergeHigherOrderArray";
+import { distinctUntilChanged, filter, map, scan } from "rxjs/operators";
+import { getStateObservable } from "./redux-observable";
+
+type Nullable<T> = T | null;
+
+// Similar to:
+// - fp-ts `catOptions`
+// - Haskell `catMaybes`
+// but operates on Observables instead of Arrays.
+export const catNullable$ = <T>(): OperatorFunction<Nullable<T>, T> =>
+  pipe(filter((t): t is T => t !== null));
+
+type RunListEpicsParams<ListItemState, Action extends ReduxAction> = {
+  action$: ActionsObservable<Action>;
+  selectListItem: (
+    id: string
+  ) => (states: { [id: string]: ListItemState }) => Nullable<ListItemState>;
+  listItemEpic: Epic<Action, Action, ListItemState>;
+};
+export const runListEpics = <ListItemState, Action extends ReduxAction>({
+  action$,
+  selectListItem,
+  listItemEpic
+}: RunListEpicsParams<ListItemState, Action>): OperatorFunction<
+  { [id: string]: ListItemState },
+  Action
+> => {
+  type ListItemAction$sById = { [id: string]: Observable<Action> };
+
+  const seed: ListItemAction$sById = {};
+
+  return listItemStates$ => {
+    const getListItemStateStateObservable = (
+      id: string,
+      initialState: ListItemState
+    ) => {
+      const listItemState$ = listItemStates$.pipe(
+        map(selectListItem(id)),
+        // When the list item is removed, this observable will emit `None` right before it
+        // is unsubscribed, so we filter this out.
+        catNullable$(),
+        distinctUntilChanged()
+      );
+      return getStateObservable(listItemState$, initialState);
+    };
+
+    return listItemStates$.pipe(
+      // When an ID is added, invoke the epic and store the result.
+      // When an ID is deleted, delete the epic result.
+      scan(
+        (
+          listItemAction$sById: ListItemAction$sById,
+          listItemStates: { [id: string]: ListItemState }
+        ) => {
+          const oldIds = Object.keys(listItemAction$sById);
+
+          const addedStates = omit(listItemStates, oldIds);
+          const added = mapValues(addedStates, (listItemState, addedId) => {
+            const listItemStateObservable$ = getListItemStateStateObservable(
+              addedId,
+              listItemState
+            );
+            const listItemAction$ = listItemEpic(
+              action$,
+              listItemStateObservable$,
+              {}
+            );
+            return listItemAction$;
+          });
+
+          const newIds = Object.keys(listItemStates);
+          const deletedIds = difference(oldIds, newIds);
+          const afterDeleted = omit(listItemAction$sById, deletedIds);
+          return {
+            ...added,
+            ...afterDeleted
+          };
+        },
+        seed
+      ),
+      map(listItemAction$sById => Object.values(listItemAction$sById)),
+      // When an observable is added, subscribe and emit values.
+      // When an observable is deleted, unsubscribe.
+      mergeHigherOrderArray()
+    );
+  };
+};
