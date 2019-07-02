b89eeea380cf0accef95b6baec7ced89b5b0a2f0
Oliver Joseph Ash <oliverjash@gmail.com>
2019-07-02 10:53:51 +0100

    Remove child epics

--- src/epics.ts
+++ src/epics.ts
@@ -1,39 +1,28 @@
 import { Epic } from "redux-observable";
 import { tag } from "rxjs-spy/operators/tag";
 import { ajax } from "rxjs/ajax";
-import { filter, map, mapTo, mergeMap, takeUntil } from "rxjs/operators";
+import { filter, mapTo, mergeMap, takeUntil } from "rxjs/operators";
 import {
   Action,
   checkIsAddFileAction,
   checkIsRemoveFileAction,
   fileUploaded
 } from "./actions";
-import { getStateObservable } from "./redux-observable";
-import { FileState, State } from "./state-types";
+import { State } from "./state-types";
 
-const fileEpic: Epic<Action, Action, FileState> = (_action$, state$) =>
-  ajax({ method: "put", url: "https://httpbin.org/put" }).pipe(
-    tag(`upload ${state$.value.id}`),
-    mapTo(fileUploaded(state$.value.id))
-  );
-
-export const rootEpic: Epic<Action, Action, State> = (action$, state$) => {
+export const rootEpic: Epic<Action, Action, State> = (action$, _state$) => {
   const addFileAction$ = action$.pipe(filter(checkIsAddFileAction));
   const removeFileAction$ = action$.pipe(filter(checkIsRemoveFileAction));
   const fileAction$ = addFileAction$.pipe(
-    // Run one child epic per file
+    // Run one Observable per file
     mergeMap(action => {
       const removeThisFileAction$ = removeFileAction$.pipe(
         filter(({ id }) => id === action.id)
       );
-      const getState = (state: State) => state.fileStates[action.id];
-      const initialState = getState(state$.value);
-      const fileStateObservable = getStateObservable(
-        state$.pipe(map(getState)),
-        initialState
-      );
-      return fileEpic(action$, fileStateObservable, {}).pipe(
-        // Dynamically unsubscribe from the child epic
+      return ajax({ method: "put", url: "https://httpbin.org/put" }).pipe(
+        tag(`upload ${action.id}`),
+        mapTo(fileUploaded(action.id)),
+        // Dynamically unsubscribe from the Observable
         takeUntil(removeThisFileAction$)
       );
     })
